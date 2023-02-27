6be2daf3bb250f82f07900bad088efdf224bdc52
tmmmarius <119420997+tmmmarius@users.noreply.github.com>
2023-02-27 21:08:34 +0100

    Update auswertung.R

--- auswertung.R
+++ auswertung.R
@@ -8,7 +8,58 @@ source("funktionen.R")
 #auf der Konsole ausgegeben
 daten = getData(str = TRUE)
 
-metrischeVariablen(daten$Alter)
+###### Funktion 3a)
+metrischeVariablen(dat$Alter)
+$arithmetisches_Mittel
+[1] 24.69
+
+$`getrimmtes artihmetisches Mittel`
+[1] 24.7375
+
+$Median
+[1] 25
+
+$`Haeufigkeitstabelle/ Modus`
+x
+19 21 22 23 24 25 26 27 28 29 
+ 1  2  7 13 18 32 11 12  3  1 
+
+$Minimum
+[1] 19
+
+$Maximum
+[1] 29
+
+$Spannweite
+[1] 10
+
+$Varianz
+[1] 2.963535
+
+$Standardabweichung
+[1] 1.721492
+
+$Variationskoeffizient
+[1] 0.06972427
+
+$Quantile
+25% 75% 
+ 24  26 
+
+$Interquartilsabstand
+[1] 2
+
+###### Funktion 3b)
+deskfun(dat$Studienfach)
+$Haeufigkeitstabelle
+x
+Data Science   Informatik        Mathe    Statistik 
+        0.39         0.27         0.06         0.28 
+
+deskfun2(dat$Studienfach)
+[1] "Data Science"
+
+###### Funktion 3c)
 
 
 
